bcfd95702284f2ee5074e0145191cb4d766c16a1
Hemanth Pulusuganti <hemanthp@propelapps.com>
2025-08-07 13:43:27 +0530

    added db.json

--- src/app/components/member/member.component.ts
+++ src/app/components/member/member.component.ts
@@ -1,17 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonCard,IonItem ,IonThumbnail,IonLabel,IonButton,IonCol,IonText,IonRow,IonIcon} from "@ionic/angular/standalone";
+import { IonCard, IonItem, IonThumbnail, IonLabel, IonButton, IonCol, IonText, IonRow, IonIcon } from "@ionic/angular/standalone";
+import { Observable } from 'rxjs';
+import { Member } from 'src/app/interfaces/member.interface';
+import { MemberService } from 'src/app/services/member/member.service';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.scss'],
-  standalone:true,
-  imports: [IonCard,IonItem,IonThumbnail,IonLabel,IonRow,IonText,IonCol ,IonButton,IonIcon]
+  standalone: true,
+  imports: [IonCard, IonItem, IonThumbnail, IonLabel, IonRow, IonText, IonCol, IonButton, IonIcon,CommonModule]
 })
-export class MemberComponent  implements OnInit {
+export class MemberComponent implements OnInit {
 
-  constructor() { }
+  listOfMembers:Member[]=[];
+  constructor(private memberService: MemberService) { }
 
-  ngOnInit() {}
+  ngOnInit() :void{ 
+    this.getListOfMembers();    
+  }
 
+  getListOfMembers(): void {
+   this.memberService.apiRequest<Member[]>('GET').subscribe({
+    next:(res)=>{
+      this.listOfMembers=res;
+    },
+  error: (err) => {
+    console.error("Error fetching members", err);
+  }
+   })
+  
+  }
 }
---
+++ src/app/services/member/member.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+  private baseUrl = 'http://localhost:3000/members/';
+
+
+  constructor(private http:HttpClient) { }
+  
+  apiRequest<T>(
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    path: string = '',
+    body?: any
+  ): Observable<T> {
+    const url = `${this.baseUrl}${path}`;
+    return this.http
+      .request<T>(method, url, { body })
+      .pipe(catchError(this.handleError));
+  }
+  handleError(error: any) {
+    console.error('Global Error Handler:', error);
+    return throwError(() => error);
+  }
+}
